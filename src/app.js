5f1406f804687b1f142e3b8e16cde6acc9f1ef84
Pratham Khanduja <prathk30@gmail.com>
2025-07-04 14:55:30 +0530

    cors fixed

--- src/app.js
+++ src/app.js
@@ -16,8 +16,10 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 
 // Error handling and cleanup middleware
